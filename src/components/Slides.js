b28743dec97155d346120b77da0443ad91bcf68b
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-15 18:49:36 -0400

    some mods

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,56 +1,75 @@
 import React, { useState, useEffect } from 'react';
 
-function Slides({slides}) {
+const Slides = ({ slides }) => {
+  const [slide, setSlide] = useState(slides[0]);
+  const [disablePrev, setDisablePrev] = useState(true);
+  const [disableNext, setDisableNext] = useState(false);
+  const [disableRestart, setDisableRestart] = useState(true);
 
-    const [slide, setSlide] = useState(slides[0]);
-    const [disablePrev, setDisablePrev] = useState(true);
-    const [disableNext, setDisableNext] = useState(false);
-    const [disableRestart, setDisableRestart] = useState(true);
+  //Total Slides
+  const slideMaxIndex = slides.length - 1;
 
-    // find total length of slide
-    const slideMaxIndex = slides.length - 1;
+  useEffect(() => {
+    const curIdx = slides.findIndex(item => item.title === slide.title);
 
-    useEffect(() => {
-        const curIdx = slides.findIndex((item) => item.title === slide.title);
-
-        if (curIdx === slideMaxIndex) {
-            setDisableNext(true);
-        }
-        else if (curIdx === 0) {
-            setDisablePrev(true);
-            setDisableRestart(true);
-        }
-        else if (curIdx > 0 && curIdx < slideMaxIndex) {
-            setDisableNext(false);
-            setDisablePrev(false);
-            setDisableRestart(false);
-        }
-    }, [slide]);
-
-    const pass = (numIdx) => {
-          setSlide(slides[slides.findIndex((item) => item.title === slide.title) + numIdx])
+    if (curIdx === slideMaxIndex) {
+      setDisableNext(true);
+    } else if (curIdx === 0) {
+      setDisablePrev(true);
+      setDisableRestart(true);
+    } else if (curIdx > 0 && curIdx < slideMaxIndex) {
+      setDisableNext(false);
+      setDisablePrev(false);
+      setDisableRestart(false);
     }
+  }, [slide.title, slides, slideMaxIndex]);
 
-    const restart = () => {
-        setDisableNext(false);
-        setDisableRestart(true);
-        setSlide(slides[0]);
-    }
-
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined" onClick={restart} disabled={disableRestart}>Restart</button>
-                <button data-testid="button-prev" className="small" disabled={disablePrev} onClick={() => {pass(-1)}}>Prev</button>
-                <button data-testid="button-next" className="small" disabled={disableNext} onClick={() => {pass(+1)}}>Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">{slide.title}</h1>
-                <p data-testid="text">{slide.text}</p>
-            </div>
-        </div>
+  const pass = numIdx => {
+    setSlide(
+      slides[slides.findIndex(item => item.title === slide.title) + numIdx]
     );
+  };
 
-}
+  const restart = () => {
+    setDisableNext(false);
+    setDisableRestart(true);
+    setSlide(slides[0]);
+  };
+
+  return (
+    <div>
+      <div id='navigation' className='text-center'>
+        <button
+          data-testid='button-restart'
+          className='small outlined'
+          onClick={restart}
+          disabled={disableRestart}
+        >
+          Restart
+        </button>
+        <button
+          data-testid='button-prev'
+          className='small'
+          onClick={() => pass(-1)}
+          disabled={disablePrev}
+        >
+          Prev
+        </button>
+        <button
+          data-testid='button-next'
+          className='small outlined'
+          onClick={() => pass(1)}
+          disabled={disableNext}
+        >
+          Next
+        </button>
+      </div>
+      <div id='slide' className='card text-center'>
+        <h1 data-testid='title'>{slide.title}</h1>
+        <p data-testid='text'>{slide.text}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Slides;
